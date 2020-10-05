536c25adcc051a2f42683965420fd812e38cec12
Nishant Kumar Singh <32234614+nishantnksingh@users.noreply.github.com>
2020-10-05 12:12:42 +0530

    Update negpos.c

--- negpos.c
+++ negpos.c
@@ -4,9 +4,6 @@ void main()
  int num;
  printf("Enter a number: \n");
  scanf("%d", &num);
- if (num > 0)
- printf("%d is a positive number \n", num);
- else 
- printf("%d is a negative number \n", num);
+ (num > 0) ? ( printf("%d is a positive number \n", num);) : (printf("%d is a negative number \n", num););
 
 }
